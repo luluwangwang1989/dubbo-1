f90b34ffa5d181f7e1da7a8343b6ac24756bb63d
Administrator <admin@example.com>
2018-04-20 14:46:43 +0800

    add

---
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
@@ -0,0 +1,30 @@
+package com.alibaba.dubbo.demo.consumer;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ken.lj on 2017/7/31.
+ */
+public class Consumer {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
+        context.start();
+
+        DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                String hello = demoService.sayHello("world"); // call remote method
+                System.out.println(hello); // get result
+
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+
+        }
+    }
+}
